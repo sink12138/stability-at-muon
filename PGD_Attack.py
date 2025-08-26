3504e6c631cbd63c5417fdaa9ab6aa19f5f1994a
mm7222 <zhaqiliang47@gmail.com>
2025-08-26 16:48:45 +0800

    Update PGD_Attack.py

--- PGD_Attack.py
+++ PGD_Attack.py
@@ -625,3 +625,78 @@ class LinfPGDAttack(Attack):
             adv_images = torch.clamp(images + delta, min=0, max=1).detach()
 
         return adv_images
+
+
+class MuonL2PGDAttack(Attack):
+    def __init__(self, model, eps=1.0, alpha=0.2, steps=10, random_start=True, 
+                 eps_for_division=1e-10, loss=nn.CrossEntropyLoss(), momentum_beta=0.95): #增加动量
+        super().__init__("MuonPGDL2", model)
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.random_start = random_start
+        self.eps_for_division = eps_for_division
+        self.supported_mode = ['default', 'targeted']
+        self.loss = loss
+        self.momentum_beta = momentum_beta  #动量初始化
+        self.momentum_buffer = None         #
+
+    def forward(self, images, labels):
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+        batch_size = len(images)
+
+        if self.targeted:
+            target_labels = self.get_target_label(images, labels)
+
+        if self.momentum_buffer is None or self.momentum_buffer.size(0) != batch_size: #初始化动量buffer
+            self.momentum_buffer = torch.zeros_like(images)
+
+        adv_images = images.clone().detach()
+
+        if self.random_start:
+            delta = torch.empty_like(adv_images).normal_()
+            d_flat = delta.view(batch_size, -1)
+            n = d_flat.norm(p=2, dim=1).view(batch_size, 1, 1, 1)
+            r = torch.zeros_like(n).uniform_(0, 1)
+            delta *= r / n * self.eps
+            adv_images = torch.clamp(adv_images + delta, min=0, max=1).detach()
+
+        for _ in range(self.steps):
+            adv_images.requires_grad = True
+            outputs = self.get_logits(adv_images)
+
+            if self.targeted:
+                cost = -self.loss(outputs, target_labels)
+            else:
+                cost = self.loss(outputs, labels)
+
+            grad = torch.autograd.grad(cost, adv_images,
+                                      retain_graph=False, create_graph=False)[0]
+
+            # muon update
+            grad = muon_update(
+                    grad=grad,
+                    momentum=self.momentum_buffer,
+                    beta=self.momentum_beta,
+                    ns_steps=self.ns_steps,
+                    nesterov=False  # 
+                ).reshape_as(grad)  # 确保梯度形状与输入匹配
+
+            # normalize
+            grad_norms = torch.norm(momentum_grad.view(batch_size, -1), p=2, dim=1) + self.eps_for_division
+            momentum_grad = momentum_grad / grad_norms.view(batch_size, 1, 1, 1)
+
+            # update
+            adv_images = adv_images.detach() + self.alpha * momentum_grad #扰动
+
+            
+            delta = adv_images - images
+            delta_norms = torch.norm(delta.view(batch_size, -1), p=2, dim=1)
+            factor = self.eps / delta_norms
+            factor = torch.min(factor, torch.ones_like(delta_norms))
+            delta = delta * factor.view(-1, 1, 1, 1)
+
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+
+        return adv_images
